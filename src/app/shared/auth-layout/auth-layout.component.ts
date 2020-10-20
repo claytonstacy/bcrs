a417dedb8c0139912e72367fbc1565be0758aa37
Clayton Stacy <clayton.stacy@ssandtech.com>
2020-10-20 11:01:16 -0500

    Setting up routing,theme,signin

--- src/app/shared/auth-layout/auth-layout.component.ts
+++ src/app/shared/auth-layout/auth-layout.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthLayoutComponent implements OnInit {
 
+  year: number = Date.now();
   constructor() { }
 
   ngOnInit(): void {
--- src/app/shared/base-layout/base-layout.component.ts
+++ src/app/shared/base-layout/base-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-base-layout',
@@ -8,10 +10,18 @@ import { Component, OnInit } from '@angular/core';
 export class BaseLayoutComponent implements OnInit {
 
   year: number = Date.now();
-  
-  constructor() { }
+  sessionUser: string;
+
+  constructor(private cookieService: CookieService, private router: Router) {
+    this.sessionUser = this.cookieService.get('session_user');
+   }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.cookieService.delete('session_user');
+    this.router.navigate(['/session/signin'])
+
+  }
 }
