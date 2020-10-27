3bae9978193087141ae218d888591fae054588dc
Clayton Stacy <clayton.stacy@ssandtech.com>
2020-10-27 17:26:04 -0500

    Finishing verify questions api

--- server/routes/session-api.js
+++ server/routes/session-api.js
@@ -55,27 +55,42 @@ router.post('/signin', async(req, res) => {
 //Verify User API goes here
 
 
-//Verify Security Question API goes here
+//Verify Security Question API
+/*
+  Verify Security question API is expecting a body with securityQuetion property, the property should be an array as follows:
+  {
+	"securityQuestions": [
+	{"questionText": "What is your favorite candy and ice cream for a blizzard?", "answerText": "Answer1"},
+	{"questionText": "What is your favorite animal?", "answerText": "Answer2"},
+	{"questionText": "What street did you grow up on?", "answerText": "Answer3"}
+	]
+}
+*/
 router.post('/verify/users/:userName/security-questions', async(req, res) => {
   try {
-    User.findOne({'userName': req.params.userName}, function(err, user) {
+    User.findOne({'userName': req.params.userName}, function(err, user) {  //Find the user object in the DB first
       if (err) {
         console.log(err);
         const verifySecurityQuestionsMongoDbErrorResponse = new ErrorResponse(500, 'Internal Server Error', err);
         res.status(500).send(verifySecurityQuestionsMongoDbErrorResponse.toObject());
-      } else {
-        console.log(user);
-        if (user) {
-          console.log('User found');
+      } else {  //Check the answers in this else block
+          let matched = true;
+          let i = 0;
+          //Iterate through the supplied responses, find the corresponding DB answers and see if the answers match, if one doesn't exit immediately and set matched to false
+           while (i < req.body.securityQuestions.length && matched === true) {
+            matched = req.body.securityQuestions[i].answerText === user.securityQuestions.find(q => q.questionText === req.body.securityQuestions[i].questionText).answerText;
+            console.log('This is matched', matched);
+            i++
+          }
+          //If all the answers passed, then send a success message
+          if (matched === true) {
+            console.log('Good responses', user)
             const verifySecurityQuestionResponse = new BaseResponse(200, 'User Found', user);
             res.json(verifySecurityQuestionResponse.toObject());
-          /* let passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
-          if (passwordIsValid && user.isEnabled) {
-             */
-          } else {
-            console.log('User not found');
-            const invalidUsernameErrorResponse = new ErrorResponse(500, 'User not found', null);
-            res.status(401).send(invalidUsernameErrorResponse.toObject());
+          } else { //If an answer failed then send a fail response
+            console.log('Bad responses')
+            const verifySecurityQuestionsMatchErrorResponse = new ErrorResponse(500, 'Answers do not match', null);
+            res.status(500).send(verifySecurityQuestionsMatchErrorResponse.toObject());
           }
         }
     });
@@ -84,13 +99,16 @@ router.post('/verify/users/:userName/security-questions', async(req, res) => {
   }
 });
 
-//Reset Password API goes here
+//Reset Password API
+/* The Reset API needs a body with a password property
+*/
 
 router.put('/users/:userName/reset-password',async(req, res) => {
+  //hash the password before sending
   let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds);
 
   try {
-    User.findOne({
+    User.findOne({ //Find the user in the database
         'userName': req.params.userName
       },
       function (error, user) {
@@ -101,7 +119,7 @@ router.put('/users/:userName/reset-password',async(req, res) => {
             "Update-password error", error);
 
           res.status(500).send(findUserErrorResponse.toObject());
-        } else {
+        } else { //Once found, update the password property and save it
             console.log(user);
 
             user.set({
