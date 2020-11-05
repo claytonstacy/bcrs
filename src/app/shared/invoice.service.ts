5f079e4fcc895e99988f1495008ac20b678462cc
Jeff Shepherd <jeffindenver2002@yahoo.com>
2020-11-05 14:03:30 -0600

    added comment

--- src/app/shared/invoice.service.ts
+++ src/app/shared/invoice.service.ts
@@ -5,31 +5,33 @@
  * Date: 11/5/2020
  * Description: invoice service
  *****************************************************************************/
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-//import Invoice interface once it is written
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+
+// import Invoice interface once it is written
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   // Once the invoice interface is made, change the type of invoice to Invoice
   createInvoice(userName: string, invoice: any): Observable<any> {
-    return this.http.post('/api/invoices/' + userName, {
-      userName: userName,
+    return this.http.post('/api/invoice/' + userName, {
+      userName,
       lineItems: invoice.lineItems,
       partsAmount: invoice.partsAmount,
       laborAmount: invoice.laborAmount,
       lineItemTotal: invoice.lineItemTotal,
       total: invoice.total
-    })
+    });
   }
 
-  findPurchasesByServiceGraph() {
-    return this.http.get('/api/invoices/purchases-graph');
+  findPurchasesByServiceGraph(): object {
+    return this.http.get('/api/invoice/purchases-graph');
   }
 }
